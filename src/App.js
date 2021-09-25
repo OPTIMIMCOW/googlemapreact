4bfb48c77e31e8a5b646517fbffcd88177d9a596
OPTIMIMCOW <kenallan18@hotmail.com>
2021-09-25 23:48:15 +0100

    Added api key

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
 
     return (
         <MyMapComponent
-            isMarkerShown={false}
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+            isMarkerShown={true}
+            googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=" + process.env.REACT_APP_GOOGLE_API_KEY}
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `100vh` }} />}
             mapElement={<div style={{ height: `100%` }} />}
