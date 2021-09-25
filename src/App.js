3e522dd4559e6d4b56c2cf4bddd0c36dd07d9ab8
OPTIMIMCOW <kenallan18@hotmail.com>
2021-09-25 14:39:08 +0100

    google maps successfully implemented.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { GoogleMap, Marker } from "react-google-maps";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const MyMapComponent = withScriptjs(withGoogleMap(({ isMarkerShown }) =>
+        <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{ lat: -34, lng: 150 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            {isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+        </GoogleMap>
+    ))
+
+    return (
+        <MyMapComponent
+            isMarkerShown={false}
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `100vh` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+        />
+    );
 }
 
 export default App;
